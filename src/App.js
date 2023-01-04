689e0c18cbbb92c1886c2cf58208ac9aa988f329
Yash <yashdahekar92@gmail.com>
2023-01-04 18:41:40 +0530

    just demo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Rohan
+          Learn with Ninja 
         </a>
       </header>
     </div>
