6b649b74b83312915186747192f747717a5c0b54
Yash <yashdahekar92@gmail.com>
2023-01-12 18:46:44 +0530

    fixing small issues

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -3,16 +3,13 @@ import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
 import "./styles/About.css"
 
-
 const AboutUs = () => {
   return (
     <div>
       <Stack padding={
-        { lg: '50px', md: '35px', sm: '25px', sx: '15px' }
+        { lg: '30px', md: '30px', sm: '25px', sx: '15px' }
       }>
-        <h2 className='header-title'>
-          Resume Builder
-        </h2>
+        <h2 className='header-title'>Resume Builder</h2>
         <Stack className='container' direction={{
           xs: "column-reverse",
           sm: "column-reverse",
@@ -32,8 +29,8 @@ const AboutUs = () => {
               lg: "25px",
             },
           }}>
-
             Build beautiful, recruiter-tested resumes in a few clicks! Our resume builder is powerful and easy to use, with a range of amazing functions. Custom-tailor resumes for any job within minutes. Increase your interview chances and rise above the competition.
+            A resume builder is an online tool that you can use to quickly create a resume. Resume builders allow you to pick a pre-formatted resume template, fill in your contact and background information, and select pre-written work history bullet points — saving you a lot of time.
             We have developed a resume builder based on feedback from thousands of users, recruiter expertise, stellar template design and the best hiring practices. The goal is simple: help you land that dream job interview! Get an advantage in the modern professional environment.
           </Typography>
           <Stack className='img' marginBottom={{
@@ -51,26 +48,26 @@ const AboutUs = () => {
               md: "28px",
               sm: '27px',
               sx: '25px'
-            }, fontWeight: '700', marginLeft: 3
+            }, fontWeight: '550', marginLeft: 3
           }}>
             Please share your with friends
           </Typography>
         </Stack>
         <Box className='icons'>
           <a href="https://web.whatsapp.com/" target='_blank' rel="noopener noreferrer">
-            <WhatsApp className='WhatsApp' sx={{ fontSize: "40px", margin: 1 }}></WhatsApp></a>
+            <WhatsApp className='WhatsApp' sx={{ fontSize: "35px", margin: 1 }}></WhatsApp></a>
           <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-            <Facebook className='Facebook' sx={{ fontSize: "40px", margin: 1 }}></Facebook></a>
+            <Facebook className='Facebook' sx={{ fontSize: "35px", margin: 1 }}></Facebook></a>
           <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
-            <LinkedIn className='LinkedIn' sx={{ fontSize: "40px", margin: 1 }}></LinkedIn></a>
+            <LinkedIn className='LinkedIn' sx={{ fontSize: "35px", margin: 1 }}></LinkedIn></a>
           <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-            <Instagram className='Instagram' sx={{ fontSize: "40px", margin: 1 }}></Instagram></a>
+            <Instagram className='Instagram' sx={{ fontSize: "35px", margin: 1 }}></Instagram></a>
           <a href="https://mail.google.com/" target="_blank" rel="noopener noreferrer">
-            <Email className='Email' sx={{ fontSize: "40px", margin: 1 }}></Email></a>
+            <Email className='Email' sx={{ fontSize: "35px", margin: 1 }}></Email></a>
           <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-            <Twitter className='Twitter' sx={{ fontSize: "40px", margin: 1 }}></Twitter></a>
+            <Twitter className='Twitter' sx={{ fontSize: "35px", margin: 1 }}></Twitter></a>
           <a href="https://www.reddit.com/" target="_blank" rel="noopener noreferrer">
-            <Reddit className='Reddit' sx={{ fontSize: "40px", margin: 1 }}></Reddit></a>
+            <Reddit className='Reddit' sx={{ fontSize: "35px", margin: 1 }}></Reddit></a>
 
         </Box>
       </Stack>
