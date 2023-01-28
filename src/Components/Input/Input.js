179b9a256c3e1caeeb4af1470408848c750964d9
Yash <yashdahekar92@gmail.com>
2023-01-28 18:00:05 +0530

    register not working properly, changed error code

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,24 +1,24 @@
 import { TextField } from '@mui/material'
 import React from 'react'
 import './Input.css'
-
-
+// import { Checks } from './Checks'
 
 const Input = (props) => {
   return (
-    <>
       <TextField
+        // error={props.error}
+        error={props.value === "" ? true : false}
         className='inputTF'
         variant='outlined'
         label={props.label}
         type={props.type}
         name={props.name} 
+        // {...props.register(props.name, Checks(props.type, props.name))}
         value={props.value}
         onChange={(event) => props.setValue(event.target.value.toString())}
-        error={props.error}
-        helperText={props.errorMessage ? props.errorMessage : null}
+        helperText={props.value ==="" ? "*Please fill this field" : null}
+        // helperText={props.errorMessage ? props.errorMessage : null}
       />
-    </>
   )
 }
 
