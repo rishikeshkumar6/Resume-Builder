51dbcf7ea1f54cd4c8f849f6277093f002e7893e
dubey123459 <gopaldubey960@gmail.com>
2023-01-31 22:16:18 +0530

    Fixed detail filling

--- src/Pages/DetailFiling.js
+++ src/Pages/DetailFiling.js
@@ -9,38 +9,23 @@ import Achievements from '../Components/Achievements/Achievements'
 import Projects from '../Components/Projects/Projects'
 import Preview from '../Components/Preview/Preview'
 
-const DetailFiling = () => {
-  const [click ,setClick] = useState(0)
-
+const DetailFiling = (props) => {
+  const [click, setClick] = useState(0)
   return (
-    <>
-    {click === 6 ? null : (
-      <div className='detailfilling'>
-     <SideBar click={click} setClick={setClick}/> 
-      {click === 0 ? (
-     <PersonalInfo click={click} setClick={setClick}/>
-      ): null}
-      {click === 1 ? (
-     <WorkExperience click={click} setClick={setClick}/>
-      ): null}
-      {click === 2 ? (
-     <Education click={click} setClick={setClick}/>
-      ): null}
-      {click === 3 ? (
-     <KeySkill click={click} setClick={setClick}/>
-      ): null}
-      {click === 4 ? (
-     <Achievements click={click} setClick={setClick}/>
-      ): null}
-      {click === 5 ? (
-     <Projects click={click} setClick={setClick}/>
-      ): null}
-     </div>
-    )}
-    {
-      click === 6 ? <Preview /> : null
-    }       
-    </>
+    <div>
+      {click === 6 ? null : (
+        <div className='detailfilling'>
+          <SideBar click={click} setClick={setClick} />
+          {click === 0 ? <PersonalInfo   click={click} setClick={setClick} /> : null}
+          {click === 1 ? <WorkExperience click={click} setClick={setClick} /> : null}
+          {click === 2 ? <Education      click={click} setClick={setClick} /> : null}
+          {click === 3 ? <KeySkill       click={click} setClick={setClick} /> : null}
+          {click === 4 ? <Achievements   click={click} setClick={setClick} /> : null}
+          {click === 5 ? <Projects       click={click} setClick={setClick} /> : null}
+        </div>
+      )}
+      {click === 6 ? <Preview click={click} setClick={setClick} /> : null}
+    </div>
   )
 }
 
