e36000ef3e0d542764748c1efc89cfc0b2e57dc2
dubey123459 <gopaldubey960@gmail.com>
2023-01-31 20:52:47 +0530

    worked on header

--- src/Templates/Components/Header.js
+++ src/Templates/Components/Header.js
@@ -1,11 +1,71 @@
 import React from 'react'
+import "./styles/Header.css"
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div style={{ backgroundColor: props.bgColor }}>
+      <div className="templateheading">
+        <div className="firsttemplateheading">
+          {props.personalInfo.profileImg.length > 0 ? (
+            <div className="imgcontainertemplate">
+              <img
+                className="profileimgtemplate"
+                src={props.personalInfo.profileImg}
+                alt="profile-img"
+              />
+            </div>
+          ) : (
+            <div
+              style={{ backgroundColor: props.primaryColor }}
+              className="imgcontainertemplate">
+              <p style={{ color: props.bgColor }} className="imgtexttemplate">
+                AM
+              </p>
+            </div>
+          )}
+          <div className="userdetailtemplate">
+            <h2
+              style={{ color: props.primaryColor }}
+              className="usernametemplate">
+              {props.personalInfo.firstName + " " + props.personalInfo.lastName}
+            </h2>
+            <p
+              style={{ color: props.secondaryColor }}
+              className="designationuser">
+              {props.workExperience[0].jobTitle}
+            </p>
+          </div>
+        </div>
+        <p
+          style={{ color: props.primaryColor }}
+          className="secondheadertemplate">
+          {props.personalInfo.address} {""}
+          <br />
+          {props.personalInfo.city} {""}
+          <br />
+          {props.personalInfo.state} {""}
+          <br />
+          {props.personalInfo.pinCode} {""}
 
-export default Header
+        </p>
+      </div>
+      <p
+        style={{ color: props.secondaryColor, marginLeft: '5px' }}
+        className="usermobilemailtemplate">
+        {props.personalInfo.mobile}
+      </p>
+      <p
+        style={{ color: props.secondaryColor, marginLeft: '5px' }}
+        className="usermobilemailtemplate">
+        {props.personalInfo.email}
+      </p>
+      <p
+        style={{ color: props.secondaryColor }}
+        className="aboutusertemplate">
+        {props.personalInfo.objective}
+      </p>
+    </div>
+  );
+};
+
+export default Header;
