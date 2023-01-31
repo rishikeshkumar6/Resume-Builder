9fecd42437f5dfa3f5daed53988f89b07b428e44
dubey123459 <gopaldubey960@gmail.com>
2023-01-31 22:09:55 +0530

    working on template1

---
+++ src/Templates/Components/defaultInputs.js
@@ -0,0 +1,39 @@
+export const defaultInputs = {
+    personal_info: {
+      first_name: "",
+      last_name: " ",
+      email: "",
+      mobile: "",
+      address: "",
+      city: "",
+      state: "",
+      pin_code: "",
+      objective:"",
+    },
+    work_experience: [
+      {
+        id: 1,
+        job_title: "",
+        organization_name: "",
+        start_year: "",
+        end_year: "",
+      },
+      {
+        id: 2,
+        job_title: "",
+        organization_name: "",
+        start_year: "",
+        end_year: "",
+      },
+    ],
+    education_details: {
+      type: "",
+      university: " ",
+      degree: "",
+      start_year: "",
+      end_year: "",
+    },
+    key_skills: ["", "", "", ""],
+    achievements: ["", "", "", ""],
+    projects: ["", "", "", ""],
+  };
